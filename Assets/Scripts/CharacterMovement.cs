f60230fb151a9a49a6dfaf7346d17005b7173922
tyl3n <123915347+amcnabbbaltar@users.noreply.github.com>
2023-02-16 02:13:42 -0500

    Update script and project version

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,8 +9,8 @@ public class CharacterMovement : MonoBehaviour
     private float jumpHeight = 1.5f;
     private float gravityValue = -9.81f;
     private CharacterController controller;
-    private float walkSpeed = 10;
-    private float runSpeed = 20;
+    private float walkSpeed = 5;
+    private float runSpeed = 8;
     private float rotationSpeed = 180;
     private Vector3 rotation;
  
@@ -22,7 +22,9 @@ public class CharacterMovement : MonoBehaviour
     public void Update()
     {
         // Making sure we dont have a Y velocity if we are grounded
+        // controller.isGrounded tells you if a character is grounded ( IE Touches the ground)
         groundedPlayer = controller.isGrounded;
+
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
@@ -31,14 +33,10 @@ public class CharacterMovement : MonoBehaviour
         // Moving the character foward according to the speed
         Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
         move = this.transform.TransformDirection(move);
-        if (Input.GetButton("Fire3"))// Left shift
-        {
-             controller.Move(move * runSpeed);
-        }
-        else
-        {
-            controller.Move(move * walkSpeed);
-        }
+
+        controller.Move(move * GetMovementSpeed());
+        // TODO add animation SetParameters for Idle/Walk/Run
+
         // Turning the character
         this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0);
         this.transform.Rotate(this.rotation);
@@ -49,8 +47,22 @@ public class CharacterMovement : MonoBehaviour
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
+        // TODO
+
          // Since there is no physics applied on character controller we have this applies to reapply gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    float GetMovementSpeed()
+    {
+        if (Input.GetButton("Fire3"))// Left shift
+        {
+            return runSpeed;
+        }
+        else
+        {
+            return walkSpeed;
+        }
     }
 }
